cfe5437fdf5faad516f645f6d5d302dd7998203b
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-24 23:49:13 -0300

    two friends tag

--- travainsta.py
+++ travainsta.py
@@ -46,19 +46,19 @@ class TravaInsta:
         errors = 0
         while(True):
             for j in range(5):
-                try:                        
-                    self.wait_a_min_or.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea')))
-                    self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea').click()
-                    self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea').send_keys(self.get_comment(count))
-                    self.wait_a_min_or.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]')))
-                    self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]').click()
-                    count+=1
-                    print(self.get_comment(count))
-                except:
-                    print("Pause for comment limit")
-                    sleep(600)
-                    self.driver.refresh()
-                    errors+=1
+            #try:                        
+                self.wait_a_min_or.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea')))
+                self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea').click()
+                self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea').send_keys(self.get_comment())
+                self.wait_a_min_or.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]')))
+                self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]').click()
+                count+=1
+                print(count)
+            #except:
+                print("Pause for comment limit")
+                sleep(600)
+                self.driver.refresh()
+                errors+=1
                 if(errors>2):
                     break
             if(errors>2):
@@ -66,8 +66,16 @@ class TravaInsta:
                     break
             sleep(300)
         print("fim")
-    def get_comment(self, count):
-        return message[count]
+
+    def get_comment(self):
+
+        index1 = randint(0,len(message)-1)
+        index2 = index1
+        while(index2 == index1):
+            index2 = randint(0,len(message)-1)
+            
+        comment = message[index1]+ " "+message[index2]
+        return comment
         
         
 
